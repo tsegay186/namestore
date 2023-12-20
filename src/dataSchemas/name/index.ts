dd4a404993cc60b5f869c6036834771b10949b9f
tsegay <tsegay.berhanu186@gmail.com>
2023-12-20 10:16:24 -0800

    modified: nameEntity interface updated

--- src/dataSchemas/name/index.ts
+++ src/dataSchemas/name/index.ts
@@ -1,10 +1,10 @@
 export interface entityName {
     id:string,
     name: string,
-    genderIdentity: 'man'|'woman'|'allGenders'|'unknown',
-    examples?: string[];
-    shorts?: string[],
+    genderIdentity: 'man'|'woman'|'person'|'unknown',
+    examples: string[];
+    shorts: string[],
     origin?: string,
-    ageStage: 'baby'|'adult'|'allAges'|'unknown',
+    ageStage: 'baby'|'adult'|'person'|'unknown',
     designation?: 'profession'|'honor'|'proper'|'unknown'
 }
