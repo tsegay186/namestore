af9edc8da84686710555d9464802e2a02635524a
tsegay <tsegay.berhanu186@gmail.com>
2024-01-02 10:47:33 -0800

    modified: code refactoring

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -2,9 +2,10 @@ import { useState } from "react"
 import './index.css'
 interface Props {
     message: string,
-    removeMessage: (message:string) => void
+    removeMessage: (message: string) => void,
+    children: any
 }
-const Dialog = ({ message, removeMessage }: Props) => {
+const Dialog = ({ message, removeMessage, children }: Props) => {
     const [isOpen] = useState(true)
 
     const handleClose = () => {
@@ -15,25 +16,20 @@ const Dialog = ({ message, removeMessage }: Props) => {
         position: 'initial',
         padding: '2px 0 2px 13px',
         height: 'fit-content',
-        borderRadius:'10px',
-        margin:'0 10px 10px 0',
-        borderStyle:'none',
-        backgroundColor:'lightgray'
+        borderRadius: '10px',
+        margin: '0 10px 10px 0',
+        borderStyle: 'none',
+        backgroundColor: 'lightgray',
+        maxWidth: '100%'
     }
     return (
         <>
             <dialog open={isOpen} onClose={handleClose} style={dialogStyle}>
-                <div style={{ display: 'flex' }}>
-                    <div style={{ display: 'flex', alignItems: 'center', paddingBottom:'6px' }}><span>{message}</span></div>
-                    <button style={{
-                        height: '30px',
-                        color: 'red',
-                        border: '1px solid green',
-                        borderRadius: '12px',
-                        borderStyle:'solid',
-                        marginLeft: '4px',
-                        width: '30px'
-                    }} className={"closeButton"} onClick={handleClose}>X</button>
+                <div style={{ display: 'flex', gap:'8px' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', paddingBottom: '6px' }}><span style={{ wordBreak: 'break-all' }}>{message}</span></div>
+                    <div>
+                    {children}
+                    </div>
                 </div>
             </dialog>
         </>
