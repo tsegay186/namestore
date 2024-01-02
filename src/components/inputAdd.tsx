c20045562b85e55bd6a1d4991d3631f9c39ce0d8
tsegay <tsegay.berhanu186@gmail.com>
2024-01-02 10:46:47 -0800

    added: fontawesom Icons

--- src/components/inputAdd.tsx
+++ src/components/inputAdd.tsx
@@ -1,17 +1,26 @@
 import { useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+interface prop {
+    adContent: (example: string) => void,
 
-const AddContent = () => {
-    
+}
+const AddContent = ({ adContent }: prop) => {
     const [content, setContent] = useState('')
+
     const addButtonStyle = { border: '1px solid green', fontFamily: '100px', borderLeftStyle: 'none', cursor: 'pointer', height: '38px', paddingTop: "0", borderLeft: '0px', borderBottomRightRadius: '8px', borderTopRightRadius: '8px' }
-    const addContent = (cont: string) => {
-        alert(cont)
-    }
+
     return (
         <>
             <div style={{ display: 'flex' }}>
-                <input id="example" className="inputElement" value={content} onChange={(e) => setContent(e.target.value)} type="text" placeholder="add Example" style={{ height: '34px', width: '65%', paddingLeft: '10px', border: '1px solid green', borderBottomLeftRadius: '6px', borderTopLeftRadius: '6px' }} />
-                <button onClick={() => addContent(content)} style={addButtonStyle} className="addShortButton"> <span style={{ fontSize: '30px', width: '100%' }}>+</span></button>
+                <input id="example" className="inpt" value={content} onChange={(e) => setContent(e.target.value)} type="text" placeholder="add Example" style={{ height: '34px', paddingLeft: '10px', border: '1px solid green', borderBottomLeftRadius: '6px', borderTopLeftRadius: '6px' }} />
+                <button onClick={() => {
+                    adContent(content)
+                    setContent('')
+                }
+                } style={addButtonStyle} className="zx"> <span style={{ fontSize: '30px', width: '100%' }}>
+                        <FontAwesomeIcon color="green" icon={faCheck}></FontAwesomeIcon>
+                    </span></button>
             </div>
         </>
     )
