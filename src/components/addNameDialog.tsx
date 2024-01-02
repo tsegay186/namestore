9b9ac605e5b1c5a62148a33256538dc6c05c73f0
tsegay <tsegay.berhanu186@gmail.com>
2024-01-02 10:46:00 -0800

    update: componenet updated in large

--- src/components/addNameDialog.tsx
+++ src/components/addNameDialog.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Dialog from "./Dialog/index";
 import { entityName } from "../dataSchemas/name"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
 const initial: entityName = {
     id: '',
     name: '',
@@ -11,12 +14,15 @@ const initial: entityName = {
     ageStage: 'unknown',
     designation: 'unknown'
 }
+const editDeleteBtn = { borderRadius: '10px', borderStyle: 'none', width: 'fit-content', height: 'fit-content', padding: '8px', margin: '2px', backgroundColor: 'none' }
+
 interface props {
     names: entityName[],
     setNames: (value: entityName[]) => void,
-    closeModal:()=> void
+    closeModal: () => void
 }
 const AddNameDialog = ({ names, setNames, closeModal }: props) => {
+    const inpt = useRef(null)
     const [nameEntity, setNameEntity] = useState<entityName>(initial)
 
     const [short, setShort] = useState('')
@@ -27,6 +33,13 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
 
     const [shortInputOpen, setShortInputOpen] = useState<boolean>(false)
     const [exampleInputOpen, setExampleInputOpen] = useState<boolean>(false)
+    const dialogRef = useRef(null);
+    const handleModalOpen = () => {
+      dialogRef.current && dialogRef.current.showModal()
+    }
+    const handleModalClose = () => {
+      dialogRef.current && dialogRef.current.close()
+    }
 
     const handleAddButtonStyle = { borderStyle: 'none', color: 'blue', cursor: 'pointer', padding: '10px 22px 10px 22px', width: 'fit-content', borderRadius: '10px' }
     const addButtonStyle = { border: '1px solid green', fontFamily: '100px', borderLeftStyle: 'none', cursor: 'pointer', height: '38px', paddingTop: "0", borderLeft: '0px', borderBottomRightRadius: '8px', borderTopRightRadius: '8px' }
@@ -48,7 +61,9 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
     }
 
     const removeExample = (value: string) => {
+        console.log(value)
         const exampless = examples.filter((exmple) => exmple !== value)
+        console.log(examples, exampless)
         setExamples(exampless)
     }
 
@@ -90,6 +105,9 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
         const { name, value } = htmlInpuElement
         const updateNameEntity = { ...nameEntity, [name]: value }
         setNameEntity(updateNameEntity)
+        if (value.length > 7 && value.length < 30) {
+            inpt.current.style.width = value.length + 'ch'
+        }
     }
 
     const handleSubmit = () => {
@@ -106,30 +124,34 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
         closeModal()
     }
 
+    const disabledButtonColor = {
+        color: 'green'
+    }
+
     return (
         <>
-            <div style={{ ...styleObect, flexDirection: "column", borderRadius: '12px' }}>
+            <div style={{ ...styleObect, minWidth: 'fit-content', flexDirection: "column", borderRadius: '12px', paddingRight: '12px' }}>
                 <div style={{ margin: '3px', ...styleObect, flexWrap: 'wrap' }} className="contentContainer">
                     <div style={styleObect}>
-                        <button disabled>
+                        <button disabled style={disabledButtonColor}>
                             I know
                         </button>
                     </div>
 
-                    <div style={{ display: 'flex', gap: 8, padding: 4 }}>
-                        <button name='ageStage' value='baby' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.ageStage === 'baby' ? 'cadetblue' : 'buttonface' }}>a Baby</button>
+                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8, padding: 4 }}>
+                        <button name='ageStage' value='baby' onClick={handleButtonClick} >a Baby</button>
                         <button name='ageStage' value='adult' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.ageStage === 'adult' ? 'cadetblue' : 'buttonface' }}>an Adult</button>
                         <button name='ageStage' value='person' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.ageStage === 'person' ? 'cadetblue' : 'buttonface' }}>a Person</button>
                     </div>
 
-                    <div style={{ display: 'flex', gap: 8, padding: 4 }}>
+                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8, padding: 4 }}>
                         <button name='genderIdentity' value='man' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.genderIdentity === 'man' ? 'cadetblue' : 'buttonface' }}>Man </button>
                         <button name='genderIdentity' value='woman' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.genderIdentity === 'woman' ? 'cadetblue' : 'buttonface' }}>Woman</button>
                         <button name='genderIdentity' value='person' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.genderIdentity === 'person' ? 'cadetblue' : 'buttonface' }}>Man & Woman</button>
                     </div>
 
                     <div style={styleObect}>
-                        <button disabled>
+                        <button disabled style={disabledButtonColor}>
                             based  on
                         </button>
                     </div>
@@ -139,30 +161,30 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
                     </div>
 
                     <div style={{ display: 'flex', gap: 8, padding: 4 }}>
-                        <button disabled>Whose</button>
+                        <button disabled style={disabledButtonColor}>Whose</button>
                     </div>
 
-                    <div style={{ display: 'flex', gap: 8, padding: 4 }}>
+                    <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', gap: 8, padding: 4 }}>
                         <button name='designation' value='profession' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.designation === 'profession' ? 'cadetblue' : 'buttonface' }}>Profession </button>
                         <button name='designation' value='honor' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.designation === 'honor' ? 'cadetblue' : 'buttonface' }}>Honor</button>
                         <button name='designation' value='proper' onClick={handleButtonClick} style={{ backgroundColor: nameEntity.designation === 'proper' ? 'cadetblue' : 'buttonface' }}>Proper</button>
                     </div>
 
                     <div style={{ display: 'flex', gap: 8, padding: 4 }}>
-                        <button disabled>
+                        <button disabled style={disabledButtonColor}>
                             Name
                         </button>
                     </div>
 
                     <div style={{ display: 'flex', gap: 8, padding: 4 }}>
-                        <button disabled>
+                        <button disabled style={disabledButtonColor}>
                             is
                         </button>
                     </div>
 
-                    <div style={{ display: 'flex', gap: 8, padding: 4 }}>
+                    <div style={{ display: 'flex', flexWrap: 'wrap', gap: 8, padding: 4 }}>
 
-                        <input name="name" value={nameEntity.name} placeholder='Name' onChange={handleInputChange} />
+                        <input ref={inpt} name="name" value={nameEntity.name} placeholder='Name' onChange={handleInputChange} style={{ paddingRight: '12px', maxWidth: '100%' }} />
 
                     </div>
                 </div>
@@ -171,11 +193,17 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
 
                     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', padding: '10px', width: 'fit-content', backgroundColor: 'whitesmoke', borderRadius: '12px', flexWrap: 'wrap', borderStyle: 'none', minWidth: '', maxWidth: '600px' }}>
 
-                        <div style={{ display: 'flex', width: 'fit-content', height: 'fit-content', flexWrap: 'wrap' }}>
+                        <div style={{ display: 'flex', width: 'fit-content', maxWidth: '100%', height: 'fit-content', flexWrap: 'wrap' }}>
                             {
                                 shorts.map((short: string, index: number) => (
-                                    <div style={{ display: 'block', borderStyle: 'none' }} key={index}>
-                                        <Dialog message={short} removeMessage={removeShort}></Dialog>
+                                    <div style={{ display: 'block', borderStyle: 'none', maxWidth: '100%' }} key={index}>
+                                        <Dialog message={short} removeMessage={removeShort}>
+                                            <button onClick={() => {
+                                                removeShort(short)
+                                            }} style={editDeleteBtn}>
+                                                <FontAwesomeIcon color="red" icon={faTrash} ></FontAwesomeIcon>
+                                            </button>
+                                        </Dialog>
                                     </div>
                                 ))
                             }
@@ -196,7 +224,15 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
                             {
                                 examples.map((example: string, index: number) => (
                                     <div style={{ display: 'block', borderStyle: 'none' }} key={index}>
-                                        <Dialog message={example} removeMessage={removeExample}></Dialog>
+                                        <Dialog message={example} removeMessage={removeExample}>
+                                            <dialog ref={dialogRef}>
+                                                <input type="text" placeholder="" value={example} />
+                                            </dialog>
+                                                                                        <button onClick={() => removeExample(example)} style={editDeleteBtn}>
+                                                <FontAwesomeIcon color="red" icon={faTrash} ></FontAwesomeIcon>
+                                            </button>
+                                        </Dialog>
+
                                     </div>
                                 ))
                             }
@@ -215,7 +251,7 @@ const AddNameDialog = ({ names, setNames, closeModal }: props) => {
                 </div>
                 <button style={{ height: '40px', borderStyle: 'none', borderRadius: '8px' }} onClick={handleSubmit}>Add Name</button>
             </div>
-            
+
         </>
     )
 
